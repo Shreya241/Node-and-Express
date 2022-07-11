43981d424ee3cbe79162634fa7d6b06563490844
Shreya <shreyaagrawal.kunra@gmail.com>
2022-07-11 18:56:15 +0530

    sync vs async

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+// const{readFile,writeFileSync, write}=require('fs');
 
+// const first=readFileSync('./content/subfolder/first.txt','utf8');
+// const second=readFileSync('./content/subfolder/second.txt','utf8');
 
-//Modules
+// console.log(first,second);
 
+// writeFileSync('./content/subfolder/result.txt',`Results - ${first},${second}`,{flag:'a'})
 
-const names=require('./name');
-const sayHi=require('./utils')
+const{readFile,writeFile, read}=require('fs');
 
-sayHi(names.john);
-sayHi('shreya');
-sayHi(names.peter);
-
-
-require("./sum");
+readFile('./content/subfolder/first.txt','utf8',(err,result)=>{
+   if(err){
+    console.log(err);
+    return;
+   }
+   const first=result;
+   readFile('./content/subfolder/second.txt','utf8',(err,result)=>{
+    if(err){
+        console.log(err);
+        return;
+       }
+       const second=result;
+       writeFile('./content/subfolder/result-async.txt',`${first},${second}`,(err,result)=>{
+        if(err){
+            console.log(err);
+            return;
+           }
+           console.log(result);
+       })
+   })
+})
---
+++ modules.js
@@ -0,0 +1,14 @@
+
+
+//Modules
+
+
+const names=require('./name');
+const sayHi=require('./utils')
+
+sayHi(names.john);
+sayHi('shreya');
+sayHi(names.peter);
+
+
+require("./sum");
