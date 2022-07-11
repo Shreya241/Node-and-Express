a401f8398f1b30b88518feab605287cb5c20ecdd
Shreya <shreyaagrawal.kunra@gmail.com>
2022-07-11 16:33:00 +0530

    initial

---
+++ basic.js
@@ -0,0 +1,11 @@
+const amount=91
+if(amount<10){
+    console.log('small');
+}else{
+    console.log("large");
+}
+
+console.log(`hey its my first node app!!`);
+
+console.log(__dirname);
+console.log(__filename);
